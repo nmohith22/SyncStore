2a2ba082268764eb540c61e9856601cdc475b36e
Mohith <n.mohith22@gmail.com>
2026-05-25 23:34:19 -0500

    chore: cleanup vendor files and add gitignore

--- backend/venv/Lib/site-packages/psnawp_api/__init__.py
+++
@@ -1,5 +0,0 @@
-"""PlayStation Network API Wrapper Python (PSNAWP) Retrieve User Information, Trophies, Game and Store data from the PlayStation Network."""
-
-from psnawp_api.psnawp import PSNAWP
-
-__all__ = ["PSNAWP"]
